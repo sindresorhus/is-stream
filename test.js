838082dfa3e9547310e2d5186016d2013b19b534
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 00:40:34 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,62 +1,57 @@
-'use strict';
-var fs = require('fs');
-var Stream = require('stream');
-var net = require('net');
-var test = require('ava');
-var tempfile = require('tempfile');
-var fn = require('./');
+import fs from 'fs';
+import Stream from 'stream';
+import net from 'net';
+import test from 'ava';
+import tempfile from 'tempfile';
+import m from './';
 
-test('isStream()', function (t) {
-	t.true(fn(new Stream.Stream()));
-	t.true(fn(new Stream.Readable()));
-	t.true(fn(new Stream.Writable()));
-	t.true(fn(new Stream.Duplex()));
-	t.true(fn(new Stream.Transform()));
-	t.true(fn(new Stream.PassThrough()));
-	t.true(fn(fs.createReadStream('test.js')));
-	t.true(fn(fs.createWriteStream(tempfile())));
-	t.true(fn(new net.Socket()));
-	t.false(fn({}));
-	t.false(fn(null));
-	t.false(fn(undefined));
-	t.false(fn(''));
-	t.end();
+test('isStream()', t => {
+	t.true(m(new Stream.Stream()));
+	t.true(m(new Stream.Readable()));
+	t.true(m(new Stream.Writable()));
+	t.true(m(new Stream.Duplex()));
+	t.true(m(new Stream.Transform()));
+	t.true(m(new Stream.PassThrough()));
+	t.true(m(fs.createReadStream('test.js')));
+	t.true(m(fs.createWriteStream(tempfile())));
+	t.true(m(new net.Socket()));
+	t.false(m({}));
+	t.false(m(null));
+	t.false(m(undefined));
+	t.false(m(''));
 });
 
-test('isStream.writable()', function (t) {
-	t.true(fn.writable(new Stream.Writable()));
-	t.true(fn.writable(new Stream.Duplex()));
-	t.true(fn.writable(new Stream.Transform()));
-	t.true(fn.writable(new Stream.PassThrough()));
-	t.true(fn.writable(fs.createWriteStream(tempfile())));
-	t.false(fn.writable(new Stream.Stream()));
-	t.false(fn.writable(new Stream.Readable()));
-	t.false(fn.writable(fs.createReadStream('test.js')));
-	t.false(fn.writable(new net.Socket()));
-	t.end();
+test('isStream.writable()', t => {
+	t.true(m.writable(new Stream.Writable()));
+	t.true(m.writable(new Stream.Duplex()));
+	t.true(m.writable(new Stream.Transform()));
+	t.true(m.writable(new Stream.PassThrough()));
+	t.true(m.writable(fs.createWriteStream(tempfile())));
+	t.false(m.writable(new Stream.Stream()));
+	t.false(m.writable(new Stream.Readable()));
+	t.false(m.writable(fs.createReadStream('test.js')));
+	t.false(m.writable(new net.Socket()));
 });
 
-test('isStream.readable()', function (t) {
-	t.true(fn.readable(new Stream.Readable()));
-	t.true(fn.readable(new Stream.Duplex()));
-	t.true(fn.readable(new Stream.Transform()));
-	t.true(fn.readable(new Stream.PassThrough()));
-	t.true(fn.readable(fs.createReadStream('test.js')));
-	t.false(fn.readable(new Stream.Stream()));
-	t.false(fn.readable(new Stream.Writable()));
-	t.false(fn.readable(fs.createWriteStream(tempfile())));
-	t.false(fn.readable(new net.Socket()));
-	t.end();
+test('isStream.readable()', t => {
+	t.true(m.readable(new Stream.Readable()));
+	t.true(m.readable(new Stream.Duplex()));
+	t.true(m.readable(new Stream.Transform()));
+	t.true(m.readable(new Stream.PassThrough()));
+	t.true(m.readable(fs.createReadStream('test.js')));
+	t.false(m.readable(new Stream.Stream()));
+	t.false(m.readable(new Stream.Writable()));
+	t.false(m.readable(fs.createWriteStream(tempfile())));
+	t.false(m.readable(new net.Socket()));
 });
 
-test('isStream.duplex()', function (t) {
-	t.true(fn.duplex(new Stream.Duplex()));
-	t.true(fn.duplex(new Stream.Transform()));
-	t.true(fn.duplex(new Stream.PassThrough()));
-	t.false(fn.duplex(new Stream.Stream()));
-	t.false(fn.duplex(new Stream.Readable()));
-	t.false(fn.duplex(new Stream.Writable()));
-	t.false(fn.duplex(fs.createReadStream('test.js')));
-	t.false(fn.duplex(fs.createWriteStream(tempfile())));
-	t.end();
+test('isStream.duplex()', t => {
+	t.true(m.duplex(new Stream.Duplex()));
+	t.true(m.duplex(new Stream.Transform()));
+	t.true(m.duplex(new Stream.PassThrough()));
+	t.false(m.duplex(new Stream.Stream()));
+	t.false(m.duplex(new Stream.Readable()));
+	t.false(m.duplex(new Stream.Writable()));
+	t.false(m.duplex(fs.createReadStream('test.js')));
+	t.false(m.duplex(fs.createWriteStream(tempfile())));
 });
